196ee8f3d6b7b2a6b4dffc371d19357ac8f6a4ec
Himanshu <hjawale@tulip-tech.com>
2023-12-13 18:48:09 +0530

    commit

---
+++ app/lib/sanity.ts
@@ -0,0 +1,11 @@
+import {createClient} from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = createClient({
+    projectId: "1u6tm53v",
+    dataset: "production",
+    apiVersion: "2023-12-13",
+    useCdn: true,
+});
+
+const builder = imageUrlBuilder
